ed8585386afa27888bc254fffd54fbde5b88fbba
Alexm0u <alexm0u.13@gmail.com>
2023-03-01 16:47:33 +0100

    Feat: First Request OK

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 const PORT = 3000;
-
 app.use(express.json());
+const db = require('./db/db.js'); 
+require('dotenv').config();
+
+
 
 // app.use(router);
 
@@ -11,3 +14,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.get('/welcome', (req, res) => {
+  return res.send("Bienvenido a mi app")
+})
