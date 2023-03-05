14b6b47a419243cdd63cb095ca44848776584dfc
Alexm0u <alexm0u.13@gmail.com>
2023-03-05 22:11:09 +0100

    FEAT: Cleaning code by Alv

--- views/userRoutes.js
+++ views/userRoutes.js
@@ -7,6 +7,7 @@ const isStaff = require('../middlewares/IsStaff');
 router.post("/user", userController.newUser);
 router.get("/user", userController.getAllUser);
 router.put("/updateuser/:id",verifyToken, userController.updateUser,);
+router.put("/updateuser/role/:id",verifyToken, isAdmin, userController.updateRole,);
 router.get("/user/findAllUsersDoctor", verifyToken, isStaff, userController.findAllUsersDoctor);
 router.get("/user/profile", verifyToken, userController.getUserbyId);
 
