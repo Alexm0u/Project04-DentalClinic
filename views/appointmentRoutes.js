208f7f6da6835c17eea59ef22407b7c0aadf0afc
alvarito101093 <alvaro101093@gmail.com>
2023-03-23 23:31:35 +0100

    refactor: appointment update work with id user now

--- views/appointmentRoutes.js
+++ views/appointmentRoutes.js
@@ -6,7 +6,7 @@ const router = require('express').Router();
 
 router.post('/appointment',verifyToken, appointmentController.newAppointment);
 router.get('/appointmentuser',verifyToken, appointmentController.showAppointmeasUser);  
-router.put('/appointmentmodify',verifyToken,isAdmin, appointmentController.updateAppointment);
+router.put('/appointment/update/:id',verifyToken, appointmentController.updateAppointment);
 router.get('/appointment/:id',verifyToken,isStaff, appointmentController.showAppointmentasDoctorByUserid);
 router.get('/appointments',verifyToken,isAdmin, appointmentController.getAllAppointment);
 router.delete('/appointment/deleteall',verifyToken,appointmentController.deleteAllAppointment);  
